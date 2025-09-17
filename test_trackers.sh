9cf51d14c7565694c655e819df577a6b5955cca3
AdySec <133128400+adysec@users.noreply.github.com>
2025-09-17 11:58:48 +0800

    Update test_trackers.sh

--- test_trackers.sh
+++ test_trackers.sh
@@ -7,7 +7,10 @@ if [ "$#" -ne 1 ]; then
 fi
 
 input_file="$1"
-output_file="trackers_best.txt"
+output_file_http="trackers_best_http.txt"
+output_file_https="trackers_best_https.txt"
+output_file_udp="trackers_best_udp.txt"
+output_file_wss="trackers_best_wss.txt"
 
 # 检查文件是否存在
 if [ ! -f "$input_file" ]; then
@@ -15,8 +18,11 @@ if [ ! -f "$input_file" ]; then
     exit 1
 fi
 
-# 清空输出文件
-> "$output_file"
+# 清空所有输出文件
+> "$output_file_http"
+> "$output_file_https"
+> "$output_file_udp"
+> "$output_file_wss"
 
 # 过滤掉包含blackstr.txt中恶意IP的URL，然后逐行处理
 {
@@ -32,7 +38,7 @@ fi
         http)
             if curl -s -f -m 1 "$tracker" &>/dev/null; then
                 echo "Success: $tracker"
-                echo "$tracker" >> "$output_file"
+                echo "$tracker" >> "$output_file_http"
             else
                 echo "Failed: $tracker"
             fi
@@ -40,7 +46,7 @@ fi
         https)
             if curl -s -f -m 1 "$tracker" &>/dev/null; then
                 echo "Success: $tracker"
-                echo "$tracker" >> "$output_file"
+                echo "$tracker" >> "$output_file_https"
             else
                 echo "Failed: $tracker"
             fi
@@ -51,7 +57,7 @@ fi
             host=$(echo "$host" | cut -d':' -f1)
             if nc -zuv -w 1 "$host" "$port" &>/dev/null; then
                 echo "Success: $tracker"
-                echo "$tracker" >> "$output_file"
+                echo "$tracker" >> "$output_file_udp"
             else
                 echo "Failed: $tracker"
             fi
@@ -59,7 +65,7 @@ fi
         wss)
             if wscat -c "$tracker" --timeout 1 &>/dev/null; then
                 echo "Success: $tracker"
-                echo "$tracker" >> "$output_file"
+                echo "$tracker" >> "$output_file_wss"
             else
                 echo "Failed: $tracker"
             fi
